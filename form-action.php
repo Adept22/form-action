cb8c2a9ac90623212534325ad8496bae757c7a95
Adept22 <asdof71@yandex.ru>
2019-01-14 22:37:09 +0300

    Update form-action.php

--- form-action.php
+++ form-action.php
@@ -1,116 +1,108 @@
 <?php
+	define('SITE_NAME', 'www.mysite.ru');
+	define('NAME_FROM', 'My Site');
+	define('EMAIL_FROM', 'info@mysite.ru');
 
-define('SITE_NAME', 'www.mysite.ru');
-define('NAME_FROM', 'My Site');
-define('EMAIL_FROM', 'info@mysite.ru');
+	require_once $_SERVER['DOCUMENT_ROOT'] . '/path/to/PHPMailer.php';
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/path/to/PHPMailer.php';
-
-function clearData(&$data) {
-	foreach ($data as $key => $value) {
-	     if(!is_array($value)) {
-	          $data[$key] = htmlspecialchars(stripslashes(trim($value)));
-	     } else {
-	          $result = clearData($value);
-	          if($result) return false;
-	     }
+	function clearData(&$data) {
+		foreach ($data as $key => $value) {
+			if(!is_array($value)) {
+				$data[$key] = htmlspecialchars(stripslashes(trim($value)));
+			} else {
+				$result = clearData($value);
+				if($result) return false;
+			}
+		}
+		return true;
 	}
-	return true;
-}
 
-function buildList($data, $wrapper = 'ol') {
-     $html = "<{$wrapper} style='margin-top: 0;'>";
+	function buildList($data, $wrapper = 'ol') {
+		$html = "<{$wrapper} style='margin-top: 0;'>";
 
-     foreach ($data as $value) {
-          if(!empty($value['value'])) {
-			$html .= "<li>";
+		foreach ($data as $value) {
+			if(!empty($value['value'])) {
+				$html .= "<li>";
 
-			if(isset($value['name']) && !empty($value['name'])) $html .= $value['name'] . ": ";
+				if(isset($value['name']) && !empty($value['name'])) $html .= $value['name'] . ": ";
 
-			if(is_array($value['value'])) {
-				$html .= "<{$wrapper} style='margin-top: 0;'>";
-				foreach($value['value'] as $val)
-		               if(!empty($val)) $html .= "<li>". $val . "</li>";
-				$html .= "</{$wrapper}>";
-	          } else {
-	               $html .= $value['value'];
+				if(is_array($value['value'])) {
+					$html .= "<{$wrapper} style='margin-top: 0;'>";
+
+					foreach($value['value'] as $val)
+						if(!empty($val)) $html .= "<li>". $val . "</li>";
+
+					$html .= "</{$wrapper}>";
+				} else {
+					$html .= $value['value'];
+				}
+
+				$html .= "</li>";
 			}
 
-			$html .= "</li>";
-          }
+			if(isset($value['children'])) {
+				$html .= "<li>";
 
-          if(isset($value['children'])) {
-			$html .= "<li>";
+				if(isset($value['name']) && !empty($value['name'])) $html .= $value['name'] . ":";
+				$html .= buildList($value['children'], $wrapper);
 
-               if(isset($value['name']) && !empty($value['name'])) $html .= $value['name'] . ":";
-               $html .= buildList($value['children'], $wrapper);
-
-			$html .= "</li>";
-          }
-     }
-
-     $html .= "</{$wrapper}>";
-
-     return $html;
-}
-
-function sendMail($to = array(), $subject, $message, $attachments = array()) {
-	$mail = new PHPMailer\PHPMailer\PHPMailer();
-     $mail->CharSet = 'UTF-8';
-     $mail->From = EMAIL_FROM;
-     $mail->FromName = NAME_FROM;
-
-	if(!empty($to)) {
-		foreach($to as $item) {
-			$mail->AddAddress($item['email'], $item['name']);
+				$html .= "</li>";
+			}
 		}
+
+		$html .= "</{$wrapper}>";
+
+		return $html;
 	}
 
-     $mail->IsHTML(true);
-     $mail->Subject = $subject;
-     $mail->Body = $message;
+	function sendMail($to = array(), $subject, $message, $attachments = array()) {
+		$mail = new PHPMailer\PHPMailer\PHPMailer();
+		$mail->CharSet = 'UTF-8';
+		$mail->From = EMAIL_FROM;
+		$mail->FromName = NAME_FROM;
 
-	if(!empty($attachments)) {
-		foreach($attachments as $attachment) {
-			$mail->addAttachment($attachment['path'], $attachment['name']);
-		}
+		if(!empty($to))
+			foreach($to as $item) $mail->AddAddress($item['email'], $item['name']);
+
+		$mail->IsHTML(true);
+		$mail->Subject = $subject;
+		$mail->Body = $message;
+
+		if(!empty($attachments))
+			foreach($attachments as $attachment) $mail->addAttachment($attachment['path'], $attachment['name']);
+
+		if (!$mail->Send()) return false;
+
+		return true;
 	}
 
-     if (!$mail->Send()) {
-          return false;
-     }
+	header('Content-Type: application/json');
 
-	return true;
-}
+	if(isset($_POST['form']) && !empty($_POST['form'])) {
+		$data = $_POST['form'];
 
-header('Content-Type: application/json');
+		clearData($data['fields']);
 
-if(isset($_POST['form']) && !empty($_POST['form'])) {
-     $data = $_POST['form'];
+		$message = "<!DOCTYPE html><head><title>Поступила заявка с сайта " . SITE_NAME . "</title></head><body>";
+		$message .= "<h1>Поступила заявка с сайта " . SITE_NAME . "</h1>";
+		$message .= "<p>Заголовок формы: " . $data['name'] . "</p>";
+		$message .= "<p>ID формы: " . $data['id'] . "</p>";
 
-     clearData($data['fields']);
+		$message .= buildList($data['fields']);
 
-     $message = "<!DOCTYPE html><head><title>Поступила заявка с сайта " . SITE_NAME . "</title></head><body>";
-     $message .= "<h1>Поступила заявка с сайта " . SITE_NAME . "</h1>";
-     $message .= "<p>Заголовок формы: " . $data['name'] . "</p>";
-     $message .= "<p>ID формы: " . $data['id'] . "</p>";
+		$message .= "</body></html>";
 
-     $message .= buildList($data['fields']);
+		$file = array();
 
-     $message .= "</body></html>";
+		if(isset($_FILES['file']) && $_FILES['file']['error'] == 0)
+			$file[] = array(
+				'path' => $_FILES['file']['tmp_name'],
+				'name' => $_FILES['file']['name']
+			);
 
-	$file = array();
+		sendMail(array(array('email' => EMAIL_FROM, 'name' => NAME_FROM)), 'Поступила заявка с сайта ' . SITE_NAME, $message, $file);
 
-     if(isset($_FILES['file']) && $_FILES['file']['error'] == 0) {
-		$file[] = array(
-			'path' => $_FILES['file']['tmp_name'],
-			'name' => $_FILES['file']['name']
-		);
+		echo json_encode(array('status' => true, 'data' => $data));
+	} else {
+		echo json_encode(array('status' => false, 'data' => $data));
 	}
-
-     sendMail(array(array('email' => EMAIL_FROM, 'name' => NAME_FROM)), 'Поступила заявка с сайта ' . SITE_NAME, $message, $file);
-
-     echo json_encode(array('status' => true, 'data' => $data));
-} else {
-	echo json_encode(array('status' => false, 'data' => $data));
-}
